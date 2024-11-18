b553ac32122e6269822a7c55b1570e7a7ba0b840
Tushar Mahajan <mahajant99@gmail.com>
2024-11-19 00:09:36 +0530

    Add button for add to cart and implement addToCart method

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -18,11 +19,18 @@ export class ProductListComponent {
     image: string;
   }[] = [];
 
-  constructor(private productService: ProductService) {
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService
+  ) {
     this.productService.getProducts().subscribe({
       next: (data) => {
         this.products = data;
       }
     });
+  }
+  
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
   }
 }
