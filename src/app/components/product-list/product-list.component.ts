b78ea31d31a5a2ef203614b168bd06abec3ae7a8
Tushar Mahajan <mahajant99@gmail.com>
2024-11-18 11:31:11 +0530

    Update product list component to display product details and use api call

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+  products: {
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+  }[] = [];
 
+  constructor(private productService: ProductService) {
+    this.productService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      }
+    });
+  }
 }
