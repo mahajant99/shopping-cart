a790cd45960f85aee977fdb26fff456c794b2826
Tushar Mahajan <mahajant99@gmail.com>
2024-11-19 11:03:23 +0530

    Update cart component to display products in cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./components/product-list/product-list.component";
+import { CartComponent } from "./components/cart/cart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent],
+  imports: [RouterOutlet, ProductListComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartItems: any[] = [];
 
+  constructor(private cartService: CartService) { }
+
+  ngOnInit() {
+    this.cartService.getCartItems().subscribe(items => {
+      this.cartItems = items;
+    });
+  }
 }
