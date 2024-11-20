bfacdbb3a345a871a5c7c02e0f5618ac5e388f4e
Tushar Mahajan <mahajant99@gmail.com>
2024-11-20 13:30:53 +0530

    Add navbar and routing for product n cart page
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from "./components/product-list/product-list.component";
-import { CartComponent } from "./components/cart/cart.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductListComponent, CartComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  cartItemCount: number = 0;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartService.getCartItems().subscribe(items => {
+      this.cartItemCount = items.length;
+    });
+  }
 }
